7191474e9a2bd9b3867eeaab5b4aff2fd179564b
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-10-19 18:28:02 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,31 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyCqlUy09dqxaoul0XmsOU9efQVTQVY5xhU",
+      authDomain: "kwitter-e9c34.firebaseapp.com",
+      databaseURL: "https://kwitter-e9c34-default-rtdb.firebaseio.com",
+      projectId: "kwitter-e9c34",
+      storageBucket: "kwitter-e9c34.appspot.com",
+      messagingSenderId: "764761031356",
+      appId: "1:764761031356:web:17cf8b4b5c0e09dd1806b9"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
+
+function send() {
+      msg = document.getElementById("msg").value ;
+      firebase.database().ref(room_name).push({
+            name:user_name ,
+            message:msg ,
+            like:0
+      }) ;
+      document.getElementById("msg").value = "" ;
+}
--- kwitter_room.js
+++ kwitter_room.js
@@ -34,10 +34,14 @@ function getData() {firebase.database().ref("/").on('value', function(snapshot) 
 
 getData();
 
-      function redirectToRoomName(name) {
-            console.log(name) ;
-            localStorage.setItem("room_name" , name) ;
-            window.location = "kwitter_page.html" ;
+function redirectToRoomName(name) {
+      console.log(name) ;
+      localStorage.setItem("room_name" , name) ;
+      window.location = "kwitter_page.html" ;
       }
 
-
+function logout() {
+      localStorage.removeItem("user_name") ;
+      localStorage.removeItem("room_name") ;
+      window.location = "index.html" ;
+      }
